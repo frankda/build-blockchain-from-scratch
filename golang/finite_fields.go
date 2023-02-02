4f578088258fa132ed9e388547d53a43f314ac44
frank.da <suyunyi@gmail.com>
2023-02-03 10:31:47 +1100

    Update finite field

--- golang/finite_fields.go
+++ golang/finite_fields.go
@@ -14,6 +14,21 @@ func FieldElement(num int, prime int) (*FieldElementStruct, error) {
 	return &FieldElementStruct{Num: num, Prime: prime}, nil
 }
 
-func (f FieldElementStruct) Equals(other *FieldElementStruct) bool {
+func (f *FieldElementStruct) Equals(other *FieldElementStruct) bool {
 	return f.Num == other.Num && f.Prime == other.Prime
+}
+
+func (f *FieldElementStruct) NotEquals(other *FieldElementStruct) bool {
+	return f.Num != other.Num || f.Prime != other.Prime
+}
+
+func (f *FieldElementStruct) Add(other *FieldElementStruct) (*FieldElementStruct, error) {
+	if f.Prime != other.Prime {
+		return nil, fmt.Errorf("cannot add two numbers in different Fields")
+	}
+	sum := f.Num + other.Num
+	for sum < 0 {
+		sum += f.Prime
+	}
+	return &FieldElementStruct{Num: sum % f.Prime, Prime: f.Prime}, nil
 }
--- golang/finite_fields_test.go
+++ golang/finite_fields_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestFieldElement(t *testing.T) {
-	tests := []struct {
+	testCases := []struct {
 		num      int
 		prime    int
 		expected *FieldElementStruct
@@ -18,18 +18,92 @@ func TestFieldElement(t *testing.T) {
 		{-1, 5, nil, fmt.Errorf("num -1 not in field range 0 to 4")},
 	}
 
-	for _, test := range tests {
-		result, err := FieldElement(test.num, test.prime)
-		if !reflect.DeepEqual(result, test.expected) || (err != nil && err.Error() != test.err.Error()) {
-			t.Errorf("FieldElement(%d, %d) = %v, %v, expected %v, %v", test.num, test.prime, result, err, test.expected, test.err)
+	for _, tt := range testCases {
+		result, err := FieldElement(tt.num, tt.prime)
+		if !reflect.DeepEqual(result, tt.expected) || (err != nil && err.Error() != tt.err.Error()) {
+			t.Errorf("FieldElement(%d, %d) = %v, %v, expected %v, %v", tt.num, tt.prime, result, err, tt.expected, tt.err)
 		}
 	}
 }
 
 func TestEquals(t *testing.T) {
-	fieldElement1, _ := FieldElement(2, 5)
-	fieldElement2, _ := FieldElement(2, 5)
-	if fieldElement1.Equals(fieldElement2) == false {
-		t.Errorf("Expected FieldElement(2, 5) equals FieldElement(2, 5)")
+	testCases := []struct {
+		a, b FieldElementStruct
+		want bool
+	}{
+		{FieldElementStruct{2, 3}, FieldElementStruct{2, 3}, true},
+		{FieldElementStruct{2, 3}, FieldElementStruct{3, 3}, false},
+		{FieldElementStruct{2, 3}, FieldElementStruct{2, 4}, false},
+	}
+	for _, tt := range testCases {
+		got := tt.a.Equals(&tt.b)
+		if got != tt.want {
+			t.Errorf("%v.Equals(%v) == %t, want %t", tt.a, tt.b, got, tt.want)
+		}
+	}
+}
+
+func TestNotEquals(t *testing.T) {
+	testCases := []struct {
+		a, b FieldElementStruct
+		want bool
+	}{
+		{FieldElementStruct{2, 3}, FieldElementStruct{2, 3}, false},
+		{FieldElementStruct{2, 3}, FieldElementStruct{3, 3}, true},
+		{FieldElementStruct{2, 3}, FieldElementStruct{2, 4}, true},
+	}
+	for _, tt := range testCases {
+		got := tt.a.NotEquals(&tt.b)
+		if got != tt.want {
+			t.Errorf("%v.NotEquals(%v) == %t, want %t", tt.a, tt.b, got, tt.want)
+		}
+	}
+}
+
+func TestAdd(t *testing.T) {
+	// Test cases
+	testCases := []struct {
+		f      *FieldElementStruct
+		other  *FieldElementStruct
+		result *FieldElementStruct
+		err    error
+	}{
+		{
+			f:      &FieldElementStruct{Num: 6, Prime: 5},
+			other:  &FieldElementStruct{Num: 2, Prime: 5},
+			result: &FieldElementStruct{Num: 3, Prime: 5},
+			err:    nil,
+		},
+		{
+			f:      &FieldElementStruct{Num: 4, Prime: 7},
+			other:  &FieldElementStruct{Num: 3, Prime: 5},
+			result: nil,
+			err:    fmt.Errorf("cannot add two numbers in different Fields"),
+		},
+		{
+			f:      &FieldElementStruct{Num: 10, Prime: 17},
+			other:  &FieldElementStruct{Num: 7, Prime: 17},
+			result: &FieldElementStruct{Num: 0, Prime: 17},
+			err:    nil,
+		},
+		{
+			f:      &FieldElementStruct{Num: -5, Prime: 17},
+			other:  &FieldElementStruct{Num: -7, Prime: 17},
+			result: &FieldElementStruct{Num: 5, Prime: 17},
+			err:    nil,
+		},
+	}
+
+	// Iterate over test cases and compare expected results with actual results
+	for _, tt := range testCases {
+		result, err := tt.f.Add(tt.other)
+		if tt.err != nil && err == nil {
+			t.Errorf("Unexpected error: got %v, want %v", err, tt.err)
+		}
+		if result == nil && tt.result != nil || result != nil && tt.result == nil {
+			t.Errorf("Unexpected result: got %v, want %v", result, tt.result)
+		} else if result != nil && tt.result != nil && (*result != *tt.result) {
+			t.Errorf("Unexpected result: got %v, want %v", result, tt.result)
+		}
 	}
 }
