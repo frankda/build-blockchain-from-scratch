2f0893ad8098e4ff400a248525c09de30ccaa863
frank.da <suyunyi@gmail.com>
2023-02-10 23:55:56 +1100

    Add signature class

--- python/secp256k1.py
+++ python/secp256k1.py
@@ -1,10 +1,11 @@
 from finite_fields import FieldElement
+from signature import Signature
 from point import Point
 
-P = 2**256 - 2**32 - 977
+P = 2**256 - 2**32 - 977  # prime
 A = 0
 B = 7
-N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141  # group nG = 0
 
 class S256Field(FieldElement):
   def __init__(self, num, prime=None):
@@ -25,3 +26,14 @@ class S256Point(Point):
   def __rmul__(self, coefficient):
     coef = coefficient % N
     return super().__rmul__(coefficient)
+
+  def verify(self, z, sig):
+    s_inv = pow(sig.s, N - 2, N)  # Fermat’s little theorem
+    u = z * s_inv % N
+    v = sig.r * s_inv % N
+    total = u * G + v * self  # uG + vP = R
+    return total.x.num == sig.r
+
+G = S256Point(
+    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
