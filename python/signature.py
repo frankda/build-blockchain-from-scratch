0e6187a2fbc4a74cbe8c3d917ae4b89472fca9c5
Frank.Da <suyunyi@gmail.com>
2023-02-15 21:47:35 +1100

    encode signature and add parse method for point

--- python/signature.py
+++ python/signature.py
@@ -5,3 +5,22 @@ class Signature:
   
   def __repr__(self):
     return 'Signature({:x},{:x})'.format(self.r, self.s)
+
+  # DER signatures
+  def der(self):
+    rbin = self.r.to_bytes(32, byteorder='big')
+    # remove all null bytes at the beginning
+    rbin = rbin.lstrip(b'\x00')
+    # if rbin has a high bit, add a \x00
+    if rbin[0] & 0x80:
+      rbin = b'\x00' + rbin
+    result = bytes([2, len(rbin)]) + rbin
+
+    sbin = self.s.to_bytes(32, byteorder='big')
+    # remove all null bytes at the beginning
+    sbin = sbin.lstrip(b'\x00')
+    # if sbin has a high bit, add a \x00
+    if sbin[0] & 0x80:
+      sbin = b'\x00' + sbin
+    result += bytes([2, len(sbin)]) + sbin
+    return bytes([0x30, len(result)]) + result
