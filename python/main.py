19a5d4d3d520e42be616c0b89ab4080950866f0d
frank.da <suyunyi@gmail.com>
2023-03-06 22:45:29 +1100

    update example

--- python/main.py
+++ python/main.py
@@ -3,6 +3,7 @@ from secp256k1 import S256Point, PrivateKey
 from point import Point
 from signature import Signature
 from serialization import endoe_base58
+from script import Script
 
 prime = 2**256 - 2**32 - 977
 a = FieldElement(0 , prime)
@@ -32,7 +33,7 @@ p3 = PrivateKey(s3)
 r = 0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6
 s = 0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
 
-sig = Signature(r, s)
+# sig = Signature(r, s)
 # print(sig.der().hex())
 
 
@@ -52,5 +53,17 @@ h1 = '7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d'
 # print(p3.point.address(compressed=True, testnet=False))
 
 # find WIF for private key
-print(p1.wif(compressed=True, testnet=True))
-print(p2.wif(compressed=False, testnet=True))
+# print(p1.wif(compressed=True, testnet=True))
+# print(p2.wif(compressed=False, testnet=True))
+
+z = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
+sec = bytes.fromhex('04887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e\
+4da568744d06c61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34')
+sig = bytes.fromhex('3045022000eff69ef2b1bd93a66ed5219add4fb51e11a840f4048\
+76325a1e8ffe0529a2c022100c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fd\
+dbdce6feab601')
+
+script_pubkey = Script([sec, 0xac])
+script_sig = Script([sig])
+combined_script = script_sig + script_pubkey
+print(combined_script.evaluate(z))
