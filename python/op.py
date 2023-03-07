f28e7526dfaec74b990cf41d9c14cfb13bf10a2d
frank.da <suyunyi@gmail.com>
2023-03-07 22:27:01 +1100

    Add op_checksig method

--- python/op.py
+++ python/op.py
@@ -6,6 +6,8 @@ from helper import (
     hash160,
     hash256,
 )
+from python.secp256k1 import S256Point
+from signature import Signature
 
 
 # tag::source3[]
@@ -656,14 +658,22 @@ def op_hash256(stack):
 
 
 def op_checksig(stack, z):
-    # check that there are at least 2 elements on the stack
-    # the top element of the stack is the SEC pubkey
-    # the next element of the stack is the DER signature
-    # take off the last byte of the signature as that's the hash_type
-    # parse the serialized pubkey and signature into objects
-    # verify the signature using S256Point.verify()
-    # push an encoded 1 or 0 depending on whether the signature verified
-    raise NotImplementedError
+    if len(stack) < 2:
+        return False
+    sec_pubkey = stack.pop()
+    der_signature = stack.pop()[:-1]
+    try:
+        point = S256Point.parse(sec_pubkey)
+        sig = Signature.parse(der_signature)
+    except (ValueError, SyntaxError):
+        return False
+    
+    if point.verify(z, sig):
+        stack.append(encode_num(1))
+    else:
+        stack.append(encode_num(0))
+    
+    return True
 
 
 def op_checksigverify(stack, z):
