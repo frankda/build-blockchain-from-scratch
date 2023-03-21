f2223551fa7b2fab6ae1251945b0cce68ebe3d64
Frank.Da <suyunyi@gmail.com>
2023-03-21 22:55:08 +1100

    Sign input

--- python/transaction.py
+++ python/transaction.py
@@ -83,6 +83,14 @@ class Tx:
         h256 = hash256(s)
         return int.from_bytes(h256, 'big')
     
+    def sign_input(self, input_index, private_key):
+        z = self.sig_hash(input_index)
+        der = private_key.sign(z).der()
+        sig = der + SIGHASH_ALL.to_bytes(1, 'big')
+        sec = private_key.point.sec()
+        self.tx_ins[input_index].script_sig = Script([sig, sec])
+        return self.verify_input(input_index)
+    
     def verify_input(self, input_index):
         tx_in = self.tx_ins[input_index]
         script_pubkey = tx_in.script_pubkey(testnet=self.testnet) 
